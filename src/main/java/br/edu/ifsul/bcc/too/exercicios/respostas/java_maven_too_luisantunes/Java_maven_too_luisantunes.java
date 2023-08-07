4cc53fb2907dbce96db9e752f4683c4dda6fee43
20222PF.CC0025 <20222PF.CC0025@PC-155979.passofundo.ifsul.local>
2023-08-07 11:41:14 -0300

    projeto inicial aula de 07/08/2023

---
+++ src/main/java/br/edu/ifsul/bcc/too/exercicios/respostas/java_maven_too_luisantunes/Java_maven_too_luisantunes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.bcc.too.exercicios.respostas.java_maven_too_luisantunes;
+
+/**
+ *
+ * @author 20222PF.CC0025
+ */
+public class Java_maven_too_luisantunes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
